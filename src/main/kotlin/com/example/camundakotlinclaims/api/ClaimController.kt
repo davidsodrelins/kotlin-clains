e07194fe5d2ca9ec281b9fe5e6b24e458b4b113e
David Sodré Lins <davidsodre_ba@hotmail.com>
2025-11-17 20:30:31 -0300

    first commit

--- src/main/kotlin/com/example/camundakotlinclaims/api/ClaimController.kt
+++ src/main/kotlin/com/example/camundakotlinclaims/api/ClaimController.kt
@@ -1,4 +1,81 @@
 package com.example.camundakotlinclaims.api
 
-class ClaimController {
+import org.camunda.bpm.engine.HistoryService
+import org.camunda.bpm.engine.RuntimeService
+import org.camunda.bpm.engine.runtime.ProcessInstance
+import org.camunda.bpm.engine.variable.Variables
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.math.BigDecimal
+
+@RestController
+@RequestMapping("/claims")
+class ClaimController(
+    private val runtimeService: RuntimeService,
+    private val historyService: HistoryService
+) {
+
+    // Isso é como o DTO do spring java normal
+    data class ClaimRequest(
+        val customerId: String,
+        val amount: BigDecimal,
+        val type: String
+    )
+
+    // Isso é como o DTO do spring java normal
+    data class ClaimResponse(
+        val processInstanceId: String,
+        val businessKey: String?,
+        val message: String = "Claim process started successfully",
+        val autoApproved: Boolean,
+        val amount: BigDecimal? = null
+    )
+
+    // Inicia o processo de sinistro
+    @PostMapping
+    // fun é como public ResponseEntity<ClaimResponse> submitClaim(@RequestBody ClaimRequest body) em Java
+    fun submitClaim(@RequestBody body: ClaimRequest): ResponseEntity<ClaimResponse> {
+
+        //val é o equivalente a final em Java e o Variables.createVariables() é o mesmo, só que em Kotlin
+        val vars = Variables.createVariables()
+            .putValue("customerId", body.customerId)
+            .putValue("amount", body.amount)
+            .putValue("type", body.type)
+
+        // Cria uma business key única para o processo
+        val businessKey = "CLAIM-${body.customerId}-${System.currentTimeMillis()}"
+
+        // o instace é o equivalente a ProcessInstance instance em Java, ele vai iniciar o processo
+        // e depois retornar a instância do processo com id e business key
+        val instance: ProcessInstance = runtimeService
+            .startProcessInstanceByKey(
+                // Chave do processo definida no modelo BPMN
+                "simple-claim-process",
+                // Business key única que criei acima
+                businessKey,
+                // Variáveis do processo que criei acima, os nomes devem coincidir com os do modelo BPMN
+                vars
+            )
+
+        // Busca a variável no histórico (porque o processo já terminou)
+        // historyService é injetado no construtor da classe, essa classe já é um bean do spring
+        val autoApproved = historyService
+            .createHistoricVariableInstanceQuery()
+            .processInstanceId(instance.id)
+                // isso aqui vai buscar a variável que foi setada no processo e retornar o valor que foi setado
+            .variableName("autoApproved")
+                // singleResult() retorna um único resultado da consulta, é o equivalente a getSingleResult() em Java
+            .singleResult()
+            ?.value as? Boolean ?: false //se for nulo, retorna false
+
+        return ResponseEntity.ok(
+            ClaimResponse(
+                processInstanceId = instance.id,
+                businessKey = instance.businessKey,
+                message = "Claim process started successfully",
+                autoApproved = autoApproved,
+                amount = if (autoApproved) body.amount else null
+            )
+        )
+    }
 }
--- src/main/kotlin/com/example/camundakotlinclaims/delegates/RegisterPaymentDelegate.kt
+++ src/main/kotlin/com/example/camundakotlinclaims/delegates/RegisterPaymentDelegate.kt
@@ -1,4 +1,24 @@
 package com.example.camundakotlinclaims.delegates
 
-class RegisterPaymentDelegate {
+import org.camunda.bpm.engine.delegate.DelegateExecution
+import org.camunda.bpm.engine.delegate.JavaDelegate
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import java.math.BigDecimal
+
+@Component
+class RegisterPaymentDelegate : JavaDelegate {
+
+    private val logger = LoggerFactory.getLogger(RegisterPaymentDelegate::class.java)
+
+
+    // aqui é onde a lógica do delegate acontece, ele pega as variáveis do processo e registra o pagamento
+    override fun execute(execution: DelegateExecution) {
+        val customerId = execution.getVariable("customerId") as String
+        val amount = execution.getVariable("amount") as BigDecimal
+
+        logger.info("Registrando pagamento para cliente={} valor={}", customerId, amount)
+
+        execution.setVariable("paymentRegistered", true)
+    }
 }
